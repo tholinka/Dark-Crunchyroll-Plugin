7a4f78606705b376ce79623f5792a659d88373d4
Tyler Holinka <1685504+tholinka@users.noreply.github.com>
2018-10-26 18:01:59 -0600

    Update all packages
    Switch to babel-preset-env as babel-preset-es2015 is deprecated

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
-var gulp     = require('gulp'),
-    zip      = require('gulp-zip'),
-    sass     = require('gulp-sass'),
-    babel    = require('gulp-babel'),
-    concat   = require('gulp-concat'),
-    postcss  = require('gulp-postcss'),
+const gulp = require('gulp'),
+    zip = require('gulp-zip'),
+    sass = require('gulp-sass'),
+    babel = require('gulp-babel'),
+    concat = require('gulp-concat'),
+    postcss = require('gulp-postcss'),
     sassLint = require('gulp-sass-lint'),
 
     del = require('del'),
@@ -16,17 +16,13 @@ var gulp     = require('gulp'),
         static: 'static/',
         build: 'build/',
         zip: 'build/dark-crunchyroll.zip'
-    },
-
-    BABEL_CONFIG = {
-        presets: ['es2015']
     };
 
-gulp.task('clean', function() {
+gulp.task('clean', function () {
     return del(PATHS.build + '/**/*');
 });
 
-gulp.task('lint', function() {
+gulp.task('lint', function () {
     return gulp
         .src(PATHS.styles)
         .pipe(sassLint())
@@ -34,44 +30,44 @@ gulp.task('lint', function() {
         .pipe(sassLint.failOnError());
 });
 
-gulp.task('styles', ['clean'], function() {
+gulp.task('styles', ['clean'], function () {
     return gulp
         .src(PATHS.styles)
         .pipe(sass().on('error', sass.logError))
-        .pipe(concat('styles.css', {newLine: "\n"}))
-        .pipe(postcss([ require('./add_important.js')() ]))
+        .pipe(concat('styles.css', { newLine: "\n" }))
+        .pipe(postcss([require('./add_important.js')()]))
         .pipe(gulp.dest(PATHS.build));
 });
 
-gulp.task('scripts:content', ['clean'], function() {
+gulp.task('scripts:content', ['clean'], function () {
     return gulp
         .src(PATHS.content_script)
-        .pipe(babel(BABEL_CONFIG))
-        .pipe(concat('content_script.js', {newLine: "\n"}))
+        .pipe(babel())
+        .pipe(concat('content_script.js', { newLine: "\n" }))
         .pipe(gulp.dest(PATHS.build));
 });
 
-gulp.task('scripts:background', ['clean'], function() {
+gulp.task('scripts:background', ['clean'], function () {
     return gulp
         .src([PATHS.background_scripts, PATHS.background_script])
-        .pipe(babel(BABEL_CONFIG))
-        .pipe(concat('background_script.js', {newLine: "\n"}))
+        .pipe(babel())
+        .pipe(concat('background_script.js', { newLine: "\n" }))
         .pipe(gulp.dest(PATHS.build));
 });
 
-gulp.task('extension:prepare', ['clean'], function() {
+gulp.task('extension:prepare', ['clean'], function () {
     return gulp
-        .src(PATHS.static + '**/*', {base: PATHS.static})
+        .src(PATHS.static + '**/*', { base: PATHS.static })
         .pipe(gulp.dest(PATHS.build));
 });
 
-gulp.task('package:zip', ['build'], function() {
+gulp.task('package:zip', ['build'], function () {
     var zipPaths = [
         PATHS.build + '**/*',
         '!' + PATHS.zip
     ];
 
-    return gulp.src(zipPaths, {base: PATHS.build})
+    return gulp.src(zipPaths, { base: PATHS.build })
         .pipe(zip('dark-crunchyroll.zip'))
         .pipe(gulp.dest(PATHS.build));
 });
@@ -79,7 +75,7 @@ gulp.task('package:zip', ['build'], function() {
 gulp.task('scripts', ['scripts:background', 'scripts:content']);
 
 gulp.task('build', ['clean', 'lint', 'styles', 'scripts', 'extension:prepare']);
-gulp.task('watch', ['build'], function() {
+gulp.task('watch', ['build'], function () {
     gulp.watch([
         PATHS.content_script,
         PATHS.background_scripts,
