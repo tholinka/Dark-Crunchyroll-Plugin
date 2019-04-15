f5145b3a1fcb08f5644a70eafe91b6fdacf5f0aa
Tyler Holinka <1685504+tholinka@users.noreply.github.com>
2019-04-15 17:16:26 -0600

    Update gulp to v4

--- gulpfile.js
+++ gulpfile.js
@@ -1,89 +1,92 @@
+const { series, parallel } = require('gulp');
 const gulp = require('gulp'),
-    zip = require('gulp-zip'),
-    sass = require('gulp-sass'),
-    babel = require('gulp-babel'),
-    concat = require('gulp-concat'),
-    postcss = require('gulp-postcss'),
-    sassLint = require('gulp-sass-lint'),
+	zip = require('gulp-zip'),
+	sass = require('gulp-sass'),
+	babel = require('gulp-babel'),
+	concat = require('gulp-concat'),
+	postcss = require('gulp-postcss'),
+	sassLint = require('gulp-sass-lint'),
 
-    del = require('del'),
+	del = require('del'),
 
-    PATHS = {
-        content_script: 'src/scripts/content.js',
-        background_scripts: 'src/scripts/background/**/*',
-        background_script: 'src/scripts/background.js',
-        styles: 'src/styles/**/*.scss',
-        static: 'static/',
-        build: 'build/',
-        zip: 'build/dark-crunchyroll.zip'
-    };
+	PATHS = {
+		content_script: 'src/scripts/content.js',
+		background_scripts: 'src/scripts/background/**/*',
+		background_script: 'src/scripts/background.js',
+		styles: 'src/styles/**/*.scss',
+		static: 'static/',
+		build: 'build/',
+		zip: 'build/dark-crunchyroll.zip'
+	};
 
-gulp.task('clean', function () {
-    return del(PATHS.build + '/**/*');
-});
+function clean() {
 
-gulp.task('lint', function () {
-    return gulp
-        .src(PATHS.styles)
-        .pipe(sassLint())
-        .pipe(sassLint.format())
-        .pipe(sassLint.failOnError());
-});
+	return del(PATHS.build + "/**/*");
+}
 
-gulp.task('styles', ['clean'], function () {
-    return gulp
-        .src(PATHS.styles)
-        .pipe(sass().on('error', sass.logError))
-        .pipe(concat('styles.css', { newLine: "\n" }))
-        .pipe(postcss([require('./add_important.js')()]))
-        .pipe(gulp.dest(PATHS.build));
-});
+function lint() {
+	return gulp
+		.src(PATHS.styles)
+		.pipe(sassLint())
+		.pipe(sassLint.format())
+		.pipe(sassLint.failOnError());
+}
 
-gulp.task('scripts:content', ['clean'], function () {
-    return gulp
-        .src(PATHS.content_script)
-        .pipe(babel())
-        .pipe(concat('content_script.js', { newLine: "\n" }))
-        .pipe(gulp.dest(PATHS.build));
-});
+function styles() {
+	return gulp
+		.src(PATHS.styles)
+		.pipe(sass().on('error', sass.logError))
+		.pipe(concat('styles.css', { newLine: "\n" }))
+		.pipe(postcss([require('./add_important.js')()]))
+		.pipe(gulp.dest(PATHS.build));
+}
 
-gulp.task('scripts:background', ['clean'], function () {
-    return gulp
-        .src([PATHS.background_scripts, PATHS.background_script])
-        .pipe(babel())
-        .pipe(concat('background_script.js', { newLine: "\n" }))
-        .pipe(gulp.dest(PATHS.build));
-});
+function scriptsBackground() {
+	return gulp
+		.src(PATHS.content_script)
+		.pipe(babel())
+		.pipe(concat('content_script.js', { newLine: "\n" }))
+		.pipe(gulp.dest(PATHS.build));
+}
 
-gulp.task('extension:prepare', ['clean'], function () {
-    return gulp
-        .src(PATHS.static + '**/*', { base: PATHS.static })
-        .pipe(gulp.dest(PATHS.build));
-});
+function scriptsContent() {
+	return gulp
+		.src([PATHS.background_scripts, PATHS.background_script])
+		.pipe(babel())
+		.pipe(concat('background_script.js', { newLine: "\n" }))
+		.pipe(gulp.dest(PATHS.build));
+}
 
-gulp.task('package:zip', ['build'], function () {
-    var zipPaths = [
-        PATHS.build + '**/*',
-        '!' + PATHS.zip
-    ];
+function extensionPrepare() {
+	return gulp
+		.src(PATHS.static + '**/*', { base: PATHS.static })
+		.pipe(gulp.dest(PATHS.build));
+}
 
-    return gulp.src(zipPaths, { base: PATHS.build })
-        .pipe(zip('dark-crunchyroll.zip'))
-        .pipe(gulp.dest(PATHS.build));
-});
+function packageZip() {
+	var zipPaths = [
+		PATHS.build + "**/*",
+		"!" + PATHS.zip,
+		"!dark-crunchyroll.zip"
+	];
+	return gulp.src(zipPaths, { base: PATHS.build })
+		.pipe(zip('dark-crunchyroll.zip'))
+		.pipe(gulp.dest(PATHS.build));
+}
 
-gulp.task('scripts', ['scripts:background', 'scripts:content']);
+exports.scripts = parallel(scriptsBackground, scriptsContent);
+exports.build = series(clean, lint, styles, exports.scripts, extensionPrepare);
+exports.package = series(exports.build, packageZip);
 
-gulp.task('build', ['clean', 'lint', 'styles', 'scripts', 'extension:prepare']);
-gulp.task('watch', ['build'], function () {
-    gulp.watch([
-        PATHS.content_script,
-        PATHS.background_scripts,
-        PATHS.background_script,
-        PATHS.styles
-    ], ['build']);
-});
 
-gulp.task('package', ['build', 'package:zip']);
+function watch() {
+	gulp.watch([
+		PATHS.content_script,
+		PATHS.background_scripts,
+		PATHS.background_script,
+		PATHS.styles
+	], exports.build);
+}
+exports.watch = series(exports.build, watch);
 
-gulp.task('default', ['watch']);
+exports.default = exports.watch;
